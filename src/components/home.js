b33c5c428b8ab042f05bf38471498cc1e7fd7f32
Kimihito Tanaka <kimi@kmd.keio.ac.jp>
2021-12-23 01:26:09 +0900

    implemented lifesycle of all scenario even the rare case

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React,{Component} from 'react';
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
+class Home extends Component{
+  constructor(props) {
+    super(props)
+    this.state = { name: "francis" }
+    console.log("1 - constructor")
+  }
+  static getDerivedStateFromProps(props, state) {
+    console.log("2 - getDerivedStateFromProps");
+    if (state.name==="Ron") {
+      return {
+        name:'Milhouse'
+      }
+    }
+    return null;
+  }
+
+  shouldComponentUpdate(nextProps,nextState) {
+    console.log("x-shouldComponentUpdate")
+    console.log(nextProps)
+    console.log(nextState)
+    if (nextState.name === "Steve") {
+      return false
+    }
+    return true
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log(prevProps);
+    console.log(prevState);
+    let age = 20;
+    return age;
+  }
+
+  render() {
+    console.log("3 - render")
+    return (
+      <div onClick={() => this.setState({ name: "Ron"})}>Update Name</div>
+    )
+  }
+
+  componentDidUpdate(prevProps,prevState, snapshot) {
+    console.log("x - componentDidUpdate");
+    console.log(prevState);
+    console.log(this.state);
+    console.log(snapshot);
+  }
+
+  componentDidMount() {
+    console.log("4 - componentDidMount")
+  }
+  
+  componentWillUnmount() {
+    console.log("5 - componentWillUnmount")
+  }
 }
 
 export default Home;
